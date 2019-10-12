593e24dc2bb13ee348dd686be6d242b252c53289
Pavel <pavelan@walla.com>
2019-10-12 18:49:21 +0300

    123

---
+++ BinaryDeserializerTry/ByteArrayParsers.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace BinaryDeserializerTry
+{
+    /// <summary>
+    /// Parsing Byte arrays to structs
+    /// </summary>
+    class ByteArrayParsers
+    {
+        /// <summary>
+        /// Parse byte array to struct as LittleEndian
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="b"> byte array </param>
+        /// <param name="strct"> struct</param>
+        public static void ParseByteArrayToStruct_LittleEndian<T>(byte[] b, ref T strct)
+        {
+            //Check if the struct parameter is Value type
+            if (!typeof(T).IsValueType)
+            {
+                throw new ArgumentException("The struct parameter is not of Value type");
+
+            }
+
+
+            int byteCounter = 0; // Counter for iterating byte array 
+
+            /*variables to store the values of the previous Field*/
+            TypeCode previosFieldTypeCode = TypeCode.Empty;
+            FieldInfo previosField;
+            dynamic value = 0;
+
+            /* Iterate through each field of the struct and set value from the byte array.
+             * If the byte array is shorter than struct an ArgumentException will occur.
+             */
+            try
+            {
+                foreach (var field in typeof(T).GetFields(BindingFlags.Instance |
+                                     BindingFlags.NonPublic |
+                                     BindingFlags.Public))
+                {
+                    TypeCode typeCode = Type.GetTypeCode(field.FieldType);
+                    switch (typeCode)
+                    {
+
+                        case TypeCode.Boolean:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToBoolean(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Char:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToChar(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Byte:
+                            field.SetValueDirect(__makeref(strct), b[byteCounter]);
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.SByte:
+                            field.SetValueDirect(__makeref(strct), (sbyte)b[byteCounter]);
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Int16:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToInt16(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.UInt16:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToUInt16(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Int32:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToInt32(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.UInt32:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToUInt32(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Int64:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToInt64(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.UInt64:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToUInt64(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Single:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToSingle(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Double:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToDouble(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Decimal:
+                            decimal decimalTemp = 0m;
+                            Int32[] int32Temp = new Int32[] { BitConverter.ToInt32(b, byteCounter),
+                            BitConverter.ToInt32(b, byteCounter + 4),
+                            BitConverter.ToInt32(b, byteCounter + 8),
+                            BitConverter.ToInt32(b, byteCounter + 12) };
+                            try
+                            {
+                                decimalTemp = new decimal(int32Temp);
+                            }
+                            catch (Exception)
+                            {
+
+                                throw;
+                            }
+                            field.SetValueDirect(__makeref(strct), decimalTemp);
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+                        case TypeCode.DateTime:
+                            field.SetValueDirect(__makeref(strct), DateTime.FromBinary(BitConverter.ToInt64(b, byteCounter)));
+                            byteCounter += 8;
+                            break;
+                        case TypeCode.String:
+                            //if(previosFieldTypeCode == TypeCode.Byte)
+                            if (new List<TypeCode> { TypeCode.Byte, TypeCode.UInt16, TypeCode.UInt32, TypeCode.UInt64 }.Contains(previosFieldTypeCode))
+                                field.SetValueDirect(__makeref(strct), System.Text.Encoding.UTF8.GetString(b, byteCounter, (int)value));
+
+                            break;
+                    }
+                    previosFieldTypeCode = typeCode;
+                    previosField = field;
+                    value = field.GetValue(strct);
+                }
+            }
+            catch (System.ArgumentException)
+            {
+
+                throw new System.ArgumentException("Byte array is shorter than struct");
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// Parse byte array to struct as BigEndian
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="b"> byte array </param>
+        /// <param name="strct"> struct</param>
+        public static void ParseByteArrayToStruct_BigEndian<T>(byte[] b, ref T strct)
+        {
+            //Check if the struct parameter is Value type
+            if (!typeof(T).IsValueType)
+            {
+                throw new ArgumentException("The struct parameter is not of Value type");
+            }
+
+
+            int byteCounter = 0; // Counter for iterating byte array 
+
+            /*variables to store the values of the previous Field*/
+            TypeCode previosFieldTypeCode = TypeCode.Empty;
+            FieldInfo previosField;
+            dynamic value = 0;
+
+            /* Iterate through each field of the struct and set value from the byte array.
+             * If the byte array is shorter than struct an ArgumentException will occur.
+             */
+            try
+            {
+                foreach (var field in typeof(T).GetFields(BindingFlags.Instance |
+                                     BindingFlags.NonPublic |
+                                     BindingFlags.Public))
+                {
+                    TypeCode typeCode = Type.GetTypeCode(field.FieldType);
+                    switch (typeCode)
+                    {
+
+                        case TypeCode.Boolean:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToBoolean(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Char:
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToChar(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Byte:
+                            field.SetValueDirect(__makeref(strct), b[byteCounter]);
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.SByte:
+                            field.SetValueDirect(__makeref(strct), (sbyte)b[byteCounter]);
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Int16:
+                            Array.Reverse(b, byteCounter, sizeof(Int16));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToInt16(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.UInt16:
+                            Array.Reverse(b, byteCounter, sizeof(UInt16));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToUInt16(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Int32:
+                            Array.Reverse(b, byteCounter, sizeof(Int32));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToInt32(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.UInt32:
+                            Array.Reverse(b, byteCounter, sizeof(UInt32));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToUInt32(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Int64:
+                            Array.Reverse(b, byteCounter, sizeof(Int64));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToInt64(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.UInt64:
+                            Array.Reverse(b, byteCounter, sizeof(UInt64));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToUInt64(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Single:
+                            Array.Reverse(b, byteCounter, sizeof(Single));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToSingle(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Double:
+                            Array.Reverse(b, byteCounter, sizeof(Double));
+                            field.SetValueDirect(__makeref(strct), BitConverter.ToDouble(b, byteCounter));
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+
+                        case TypeCode.Decimal:
+                            Array.Reverse(b, byteCounter, sizeof(Decimal));
+                            decimal decimalTemp = 0m;
+                            Int32[] int32Temp = new Int32[] { BitConverter.ToInt32(b, byteCounter),
+                            BitConverter.ToInt32(b, byteCounter + 4),
+                            BitConverter.ToInt32(b, byteCounter + 8),
+                            BitConverter.ToInt32(b, byteCounter + 12) };
+                            try
+                            {
+                                decimalTemp = new decimal(int32Temp);
+                            }
+                            catch (Exception)
+                            {
+
+                                throw;
+                            }
+                            field.SetValueDirect(__makeref(strct), decimalTemp);
+                            byteCounter += Marshal.SizeOf(field.FieldType);
+                            break;
+                        case TypeCode.DateTime:
+                            Array.Reverse(b, byteCounter, sizeof(Int64));
+                            field.SetValueDirect(__makeref(strct), DateTime.FromBinary(BitConverter.ToInt64(b, byteCounter)));
+                            byteCounter += 8;
+                            break;
+                        case TypeCode.String:
+                            Array.Reverse(b, byteCounter, (int)value);
+                            if (new List<TypeCode> { TypeCode.Byte, TypeCode.UInt16, TypeCode.UInt32, TypeCode.UInt64 }.Contains(previosFieldTypeCode))
+                                field.SetValueDirect(__makeref(strct), System.Text.Encoding.UTF8.GetString(b, byteCounter, (int)value));
+
+                            break;
+                    }
+                    previosFieldTypeCode = typeCode;
+                    previosField = field;
+                    value = field.GetValue(strct);
+                }
+            }
+            catch (System.ArgumentException)
+            {
+
+                throw new System.ArgumentException("Byte array is shorter than the struct");
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// Return struct size in bytes.
+        /// Ignores fields that not value type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="strct"></param>
+        /// <returns></returns>
+        public static int GetStructSize<T>(T strct)
+        {
+            if (!typeof(T).IsValueType)
+            {
+                throw new ArgumentException("The struct parameter is not of Value type");
+            }
+
+            int byteCounter = 0;
+            foreach (var field in typeof(T).GetFields(BindingFlags.Instance |
+                                    BindingFlags.NonPublic |
+                                    BindingFlags.Public))
+            {
+                if ((field.FieldType).IsValueType)
+                    byteCounter += Marshal.SizeOf(field.FieldType);
+            }
+
+            return byteCounter;
+        }
+
+        /// <summary>
+        /// Print struct fields and values to Console.
+        /// </summary>
+        public static void IterateStruct<T>(ref T strct)
+        {
+            if (!typeof(T).IsValueType)
+            {
+                throw new ArgumentException("The struct parameter is not of Value type");
+            }
+
+            foreach (var field in typeof(T).GetFields(BindingFlags.Instance |
+                                                 BindingFlags.NonPublic |
+                                                 BindingFlags.Public))
+            {
+                Console.WriteLine("{0} = {1}", field.Name, field.GetValue(strct));
+            }
+        }
+
+    }
+}
---
+++ BinaryDeserializerTry/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+
+namespace BinaryDeserializerTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStruct testStruct = new TestStruct();
+            decimal x = 44.55m;
+            byte[] byteArraywithDecimal = GetBytes(x);
+            byte[] byteArray = new byte[] { 0x30, 0x31, 0x42 };
+            DateTime dt = new DateTime();
+            UInt32 num1 = 4;
+            byte[] bytesStringnum = BitConverter.GetBytes(num1);
+            byte[] bytesString = Encoding.ASCII.GetBytes("This is some text");
+            var s = new MemoryStream();
+            s.Write(bytesStringnum, 0, bytesStringnum.Length);
+            s.Write(bytesString, 0, bytesString.Length);
+            byte[] bytesStringX = s.ToArray();
+
+            
+            dt = DateTime.Now;
+            byte[] byteArrayTime = BitConverter.GetBytes(dt.Ticks);
+
+            ByteArrayParsers.ParseByteArrayToStruct_LittleEndian(bytesStringX, ref testStruct);
+
+            Console.WriteLine($"StructSize is {ByteArrayParsers.GetStructSize(testStruct)}");
+
+            ByteArrayParsers.IterateStruct(ref testStruct);
+        }
+
+        public static byte[] GetBytes(decimal dec)
+        {
+            //Load four 32 bit integers from the Decimal.GetBits function
+            Int32[] bits = decimal.GetBits(dec);
+            //Create a temporary list to hold the bytes
+            List<byte> bytes = new List<byte>();
+            //iterate each 32 bit integer
+            foreach (Int32 i in bits)
+            {
+                //add the bytes of the current 32bit integer
+                //to the bytes list
+                bytes.AddRange(BitConverter.GetBytes(i));
+            }
+            //return the bytes list as an array
+            return bytes.ToArray();
+        }
+    }
+
+    public struct TestStruct 
+    {
+
+        UInt16 ui1;
+        String str1;
+    }
+}
